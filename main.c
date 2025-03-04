115be60b1b798bc1eaffde65bca0fb917d142675
Ilya Sidoruk <i.sidoruk03@gmail.com>
2025-03-05 02:00:30 +0300

    init: hello world C and CPP

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, World!\n");
+
+    printf("Press Enter to exit...");
+    getchar();
+
+    return 0;
+}
---
+++ mainCPP.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main()
+{
+    std::vector<std::string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const std::string &word : msg)
+    {
+        std::cout << word << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Press Enter to exit...";
+    std::cin.get();
+
+    return 0;
+}
